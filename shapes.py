a7de7cafdcf1e4f38a266475c0cdae34bec00df3
Bryan <zhongzbryan@gmail.com>
2024-11-30 00:44:46 -0500

    Added a lot more well known shapes

--- shapes.py
+++ shapes.py
@@ -4,154 +4,189 @@
 
 import random
 
-# Glider
-glider = {
+# Block (Still Life)
+block = {
     (0, 0): True,
+    (0, 1): True,
     (1, 0): True,
-    (2, 0): True,
+    (1, 1): True,
+}
+
+# Beehive (Still Life)
+beehive = {
+    (1, 0): True,
+    (2, 1): True,
+    (2, 2): True,
+    (1, 3): True,
+    (0, 2): True,
+    (0, 1): True,
+}
+
+# Loaf (Still Life)
+loaf = {
+    (1, 0): True,
+    (2, 1): True,
+    (2, 2): True,
+    (1, 3): True,
+    (0, 2): True,
+    (0, 1): True,
+    (3, 2): True,
+}
+
+# Boat (Still Life)
+boat = {
+    (0, 0): True,
+    (0, 1): True,
+    (1, 0): True,
+    (1, 2): True,
+    (2, 1): True,
+}
+
+# Tub (Still Life)
+tub = {
+    (1, 0): True,
+    (0, 1): True,
     (2, 1): True,
     (1, 2): True,
 }
 
-# Blinker (Oscillator)
+# Blinker (Oscillator, Period 2)
 blinker = {
     (0, 0): True,
     (1, 0): True,
     (2, 0): True,
 }
 
-# Toad (Oscillator)
+# Toad (Oscillator, Period 2)
 toad = {
     (0, 1): True,
-    (1, 0): True,
-    (1, 1): True,
-    (2, 0): True,
-    (2, 1): True,
-    (3, 0): True,
-}
-
-# Pulsar (Oscillator)
-pulsar = {
-    (5, 3): True,
-    (5, 4): True,
-    (5, 5): True,
-    (5, 6): True,
-    (5, 7): True,
-    (3, 5): True,
-    (4, 5): True,
-    (6, 5): True,
-    (7, 5): True,
-    (3, 9): True,
-    (4, 9): True,
-    (6, 9): True,
-    (7, 9): True,
-    (5, 11): True,
-    (5, 12): True,
-    (5, 13): True,
-    (5, 14): True,
-    (5, 15): True,
-    (9, 5): True,
-    (9, 6): True,
-    (9, 7): True,
-    (9, 8): True,
-    (9, 9): True,
-}
-
-# Lightweight spaceship
-lws = {
-    (1, 0): True,
-    (2, 0): True,
-    (0, 1): True,
     (0, 2): True,
-    (2, 3): True,
-}
-
-# Diehard
-dieHard = {
-    (0, 0): True,
-    (0, 1): True,
-    (1, 1): True,
-    (2, 1): True,
-    (2, 2): True,
-}
-
-# Acorn
-acorn = {
-    (1, 1): True,
-    (2, 1): True,
-    (2, 2): True,
-    (2, 3): True,
-    (3, 0): True,
-    (3, 1): True,
-}
-
-# Beacon
-beacon = {
     (1, 0): True,
-    (2, 0): True,
-    (1, 1): True,
+    (1, 3): True,
     (2, 1): True,
-    (4, 2): True,
-    (5, 2): True,
-    (4, 3): True,
-    (5, 3): True,
+    (2, 2): True,
 }
 
-# R-pentomino
-rPentomino = {
+# Beacon (Oscillator, Period 2)
+beacon = {
     (0, 0): True,
     (0, 1): True,
+    (1, 0): True,
+    (1, 1): True,
+    (2, 2): True,
+    (2, 3): True,
+    (3, 2): True,
+    (3, 3): True,
+}
+
+# Pulsar (Oscillator, Period 3)
+pulsar = {
+    # Central block (with spacing)
+    (2, 4): True,
+    (3, 4): True,
+    (4, 4): True,
+    (2, 6): True,
+    (3, 6): True,
+    (4, 6): True,
+    (2, 8): True,
+    (3, 8): True,
+    (4, 8): True,
+    # Top arms
+    (0, 5): True,
+    (0, 6): True,
+    (0, 7): True,
+    (6, 5): True,
+    (6, 6): True,
+    (6, 7): True,
+    # Bottom arms
+    (8, 5): True,
+    (8, 6): True,
+    (8, 7): True,
+    (14, 5): True,
+    (14, 6): True,
+    (14, 7): True,
+}
+
+# Glider (Spaceship)
+glider = {
+    (0, 2): True,
     (1, 0): True,
     (1, 2): True,
     (2, 1): True,
+    (2, 2): True,
 }
 
-# Glider Gun
-gliderGun = {
+# Lightweight Spaceship (LWSS)
+lwss = {
+    (0, 1): True,
+    (0, 4): True,
     (1, 0): True,
-    (2, 0): True,
-    (1, 1): True,
-    (2, 1): True,
-    (11, 0): True,
-    (11, 1): True,
-    (12, 0): True,
-    (12, 1): True,
-    (9, 2): True,
-    (10, 2): True,
-    (8, 3): True,
-    (11, 4): True,
-    (9, 5): True,
-    (10, 5): True,
-    (8, 6): True,
-    (11, 7): True,
-    (9, 8): True,
-    (10, 8): True,
-    (9, 10): True,
-    (10, 10): True,
-    (7, 11): True,
-    (12, 11): True,
-    (13, 12): True,
-    (7, 13): True,
-    (12, 13): True,
-    (6, 14): True,
-    (14, 14): True,
-    (6, 15): True,
-    (7, 16): True,
-    (8, 16): True,
+    (1, 4): True,
+    (2, 4): True,
+    (3, 0): True,
+    (3, 1): True,
+    (3, 2): True,
+    (3, 3): True,
 }
+
+# Middleweight Spaceship (MWSS)
+mwss = {
+    (0, 1): True,
+    (0, 5): True,
+    (1, 0): True,
+    (1, 5): True,
+    (2, 5): True,
+    (3, 0): True,
+    (3, 1): True,
+    (3, 2): True,
+    (3, 3): True,
+    (3, 4): True,
+}
+
+# Heavyweight Spaceship (HWSS)
+hwss = {
+    (0, 1): True,
+    (0, 6): True,
+    (1, 0): True,
+    (1, 6): True,
+    (2, 6): True,
+    (3, 0): True,
+    (3, 1): True,
+    (3, 2): True,
+    (3, 3): True,
+    (3, 4): True,
+    (3, 5): True,
+}
+
+# Pentadecathlon (Oscillator, Period 15)
+pentadecathlon = {
+    (1, 0): True,
+    (1, 1): True,
+    (1, 2): True,
+    (1, 4): True,
+    (1, 5): True,
+    (1, 6): True,
+    (0, 3): True,
+    (2, 3): True,
+}
+
 
 # List of all shapes
 shapes = [
-    glider,
+    block,
+    beehive,
+    loaf,
+    boat,
+    tub,  # Still Lifes
     blinker,
     toad,
-    pulsar,
-    lws,
-    dieHard,
-    acorn,
     beacon,
-    rPentomino,
-    gliderGun,
+    pulsar,
+    pentadecathlon,  # Oscillators
+    glider,
+    lwss,
+    mwss,
+    hwss,  # Spaceships
 ]
 
 
